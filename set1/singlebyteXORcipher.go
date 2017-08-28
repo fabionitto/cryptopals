d7d379292a9c3c59ca0e7e25eac7b7f261b6f714
Fabio <fabio.nitto@planejamento.gov.br>
2017-08-28 14:00:38 -0300

    implementation of chiSquared function

--- set1/singlebyteXORcipher.go
+++ set1/singlebyteXORcipher.go
@@ -84,9 +84,67 @@ func englishScore(text string) float64 {
     }
 
     for key, value := range engFreqTable {
-//        fmt.Printf("Score: %f Key: %c , Value: %f, textValue: %f\n", score, key, value-textFreqTable[key], textFreqTable[key])
-
         score += math.Abs(value - textFreqTable[key])
+    }
+
+    for key, _ := range textFreqTable {
+        if (key < 20) || (key > 126) {
+            /* Penalty */
+            score += 1.0
+        }
+    }
+
+    return score
+}
+
+func chiChi(text string) float64 {
+    score := 0.0
+    engFreqTable := map[byte]float64{
+        'a': 8.167,
+        'b': 1.492,
+        'c': 2.782,
+        'd': 4.253,
+        'e': 12.702,
+        'f': 2.228,
+        'g': 2.015,
+        'h': 6.094,
+        'i': 6.966,
+        'j': 0.153,
+        'k': 0.772,
+        'l': 4.025,
+        'm': 2.406,
+        'n': 6.749,
+        'o': 7.507,
+        'p': 1.929,
+        'q': 0.095,
+        'r': 5.987,
+        's': 6.327,
+        't': 9.056,
+        'u': 2.758,
+        'v': 0.978,
+        'w': 2.360,
+        'x': 0.150,
+        'y': 1.974,
+        'z': 0.074,
+    }
+    textFreqTable := make(map[byte]int)
+
+    size := len(text)
+
+    lower := strings.ToLower(text)
+
+    for i:=0; i < size; i++ {
+        textFreqTable[lower[i]] = textFreqTable[lower[i]]+1
+    }
+
+    for k, v := range textFreqTable {
+        expected := float64(size) * engFreqTable[k]
+        if engFreqTable[k] > 0 {
+            score += math.Pow((float64(v)- expected),2)/expected
+        } else {
+            score += math.Pow((float64(v)- expected),2)/0.00000001 
+        }
+        //fet.Printf("%c, %d, %f, %f\n", k,textFreqTable[k], expected, score)
     }
 
     return score
@@ -105,21 +163,21 @@ func analyzeSingleByteXor(text string) byte {
 /* Analyzes text, assuming its a singlebyteXored encrypted text, and returns the most probable char as the key */
     keyScore := make(map[int]float64)
 
-    for i := 0 ; i <= 127 ; i++ {
-        fmt.Printf("char: %c\n", i)
+    for i := 65 ; i <= 127 ; i++ {
         key := expandByte(byte(i), len(text))
 
         decryptedText := fixedXor([]byte(key), []byte(text))
-        keyScore[i] = englishScore(string(decryptedText))
+        keyScore[i] = chiChi(string(decryptedText))
+        fmt.Printf("Key: %c, ChiChi: %f\n", i, keyScore[i])
     }
 
     result := 0
-    score := 100.0
+    score := keyScore['a']
     for k,v := range keyScore {
         if v < score {
-            score = v 
+            score = v
+            result = k
         }
-        result = byte(k)
     }
 
     return byte(result)
@@ -134,6 +192,10 @@ func main () {
         log.Fatal(err)
     }
 
-    _ = analyzeSingleByteXor(string(r))
+    k := analyzeSingleByteXor(string(r))
+    key := expandByte(k, len(r))
 
+    decryptedText := fixedXor([]byte(key), []byte(r))
+
+    fmt.Printf("The char key is: %c and the decrypted text is probably: %s.\n", k, decryptedText)
 }
